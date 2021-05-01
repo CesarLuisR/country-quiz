225093ae627b9cd72ef04a71058e1383dad9b9c5
Cesar Luis Rijo <cesarluisrc@gmail.com>
2021-04-30 22:17:38 -0400

    useRandomQuestion refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import useRandomQuestion from "./components/hook/useRandomQuestion";
 
 const App: React.FC = (): JSX.Element => {
   const data = useRandomQuestion();
 
   if (data) console.log(data);
-  
-  return (
-    <div>
-      Buenos dias
-    </div>
-  );
+
+  return <div>Buenos dias</div>;
 };
 
 export default App;
--- src/components/hook/useRandomQuestion.tsx
+++ src/components/hook/useRandomQuestion.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useRandomQuestion = (): object | undefined => {
+const useRandomQuestion = (): object | any => {
+  const URL: string = "https://restcountries.eu/rest/v2/all";
   const [question, setQuestion] = useState<object | undefined>();
 
   const getRandomNumber = (min: number, max: number): number => {
@@ -9,22 +10,18 @@ const useRandomQuestion = (): object | undefined => {
   };
 
   const generateQuestion = async (): Promise<void> => {
-    const randomQuestionNumber: number = getRandomNumber(0, 250);
-    const randomPossibleQuestionNumber: number = getRandomNumber(0, 2);
+    const resp = await axios.get<object[] | any>(URL);
 
-    const resp = await axios.get("https://restcountries.eu/rest/v2/all");
+    const length: number = resp.data.length - 1;
+    const randomQuestionNumber: number = getRandomNumber(0, length);
+
     const location = await resp.data[randomQuestionNumber];
 
-    const possibleQuestions: Array<string> = [
-      "Which country does this flag belong to?",
-      `${location.capital} is the capital of`,
-      `What region does ${location.name} belong to?`,
-    ];
-
+    // falses answers
     let falseAnswers: Array<object> = [];
 
     for (let i: number = 0; i < 3; i++) {
-      const newRandomQuestionNumber = getRandomNumber(0, 250);
+      const newRandomQuestionNumber = getRandomNumber(0, length);
       let randomLocation = resp.data[newRandomQuestionNumber];
       falseAnswers.push({
         capital: randomLocation.capital,
@@ -32,15 +29,69 @@ const useRandomQuestion = (): object | undefined => {
         region: randomLocation.region,
       });
     }
+    interface Ifilter {
+      type: string;
+      falseAnswers: object[];
+      trueAnswer: string | any;
+    }
 
-    setQuestion({
-      questionText: possibleQuestions[randomPossibleQuestionNumber],
-      name: location.name,
-      capital: location.capital,
-      flag: location.flag,
-      region: location.region,
-      falseAnswer: falseAnswers,
-    });
+    const filterAndSortAnswers = ({
+      type,
+      falseAnswers,
+      trueAnswer,
+    }: Ifilter): Array<string> => {
+      const answers = [...falseAnswers, trueAnswer];
+      let filterAnswers: Array<string> = [""];
+
+      if (type === "name")
+        filterAnswers = answers.map((answer) => answer.name || answer);
+      else if (type === "capital")
+        filterAnswers = answers.map((answer) => answer.capital || answer);
+      else filterAnswers = answers.map((answer) => answer.region || answer);
+
+      return filterAnswers.sort();
+    };
+
+    const possibleQuestions: Array<object> = [
+      {
+        text: "Which country does this flag belong to?",
+        type: "name",
+        correct: location.name,
+        flag: location.flag,
+        answers: filterAndSortAnswers({
+          type: "name",
+          falseAnswers,
+          trueAnswer: location.name,
+        }),
+      },
+      {
+        text: `${location.capital} is the capital of`,
+        type: "capital",
+        correct: location.name,
+        flag: false,
+        answers: filterAndSortAnswers({
+          type: "name",
+          falseAnswers,
+          trueAnswer: location.name,
+        }),
+      },
+      {
+        text: `What region does ${location.name} belong to?`,
+        type: "region",
+        correct: location.region,
+        flag: false,
+        answers: filterAndSortAnswers({
+          type: "region",
+          falseAnswers,
+          trueAnswer: location.region,
+        }),
+      },
+    ];
+
+    let newLength = possibleQuestions.length - 1;
+    const randomPossibleQuestionNumber: number = getRandomNumber(0, newLength);
+
+    setQuestion(possibleQuestions[randomPossibleQuestionNumber]);
   };
 
   useEffect(() => {
@@ -48,7 +99,7 @@ const useRandomQuestion = (): object | undefined => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (question) return question;;
+  if (question) return question;
 };
 
 export default useRandomQuestion;
